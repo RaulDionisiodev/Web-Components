0f0806bb42235638437e8595f4d0813121486017
Raul Dionisio <raul.dionisio@cortex-intelligence.com>
2023-09-08 18:25:42 -0300

    Refatorando o cardNews para que ele fique mais profissional

--- src/components/cardNews.js
+++ src/components/cardNews.js
@@ -1,11 +1,15 @@
 class CardNews extends HTMLElement {
     constructor() {
         super();
-        const shadow = this.attachShadow({ mode: "open" }) // o mode diz se o componente pode ser manipulado
+        const shadow = this.attachShadow({ mode: "open" }); // o mode diz se o componente pode ser manipulado
         shadow.innerHTML = "<h1>Hello world</h1>"
     }
+
+    build() { }
+
+    styles() { }
 }
 
-customElements.define('card-news', CardNews)
+customElements.define('card-news', CardNews) //as tags customizadas precisam ter sempre o hifén no nome
 
 // Um componente é uma função JS que retorna HTML ou JS
