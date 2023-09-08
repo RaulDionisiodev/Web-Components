225b64839969fbdbc193dbc078e5d578a580b555
Raul Dionisio <raul.dionisio@cortex-intelligence.com>
2023-09-08 18:16:57 -0300

    Componente estilizado

---
+++ src/components/TituloDinamico.js
@@ -0,0 +1,28 @@
+class TituloDinamico extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: "open" });
+
+        //base
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = "Título da notícia";
+
+        //estilo - todo estilo que eu crio na shadow dom só vale para a shadow dom e para o componente específico
+        // o comportamento do componente é isolado do restante (scoped)
+        const style = document.createElement("style");
+        style.textContent = `
+            h1 {
+                color: red;
+
+            }
+        `;
+
+        //enviar para o shadow DOM
+        shadow.appendChild(componentRoot)
+        shadow.appendChild(style)
+
+    }
+
+}
+
+customElements.define('titulo-dinamico', TituloDinamico) //as tags customizadas precisam ter sempre o hifén no nome
