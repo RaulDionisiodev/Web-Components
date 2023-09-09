c91895af284ca1960dc96e3dec635a9167695cb0
Raul Dionisio <rauldionisiosh@gmail.com>
2023-09-09 12:04:38 -0300

    Componente com build e dados dinâmicos

--- src/components/cardNews.js
+++ src/components/cardNews.js
@@ -2,10 +2,44 @@ class CardNews extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: "open" }); // o mode diz se o componente pode ser manipulado
-        shadow.innerHTML = "<h1>Hello world</h1>"
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
     }
 
-    build() { }
+    build() { 
+        const componentRoot = document.createElement('div');
+        componentRoot.setAttribute('class', 'card');
+
+        const cardLeft =  document.createElement('div');
+        cardLeft.setAttribute("class", "card__left");
+
+        const author = document.createElement('span');
+        author.textContent = "By " + (this.getAttribute('autor') || "Anonimous");
+
+        const linkTitle = document.createElement('a');
+        linkTitle.textContent = this.getAttribute('title');
+        linkTitle.href = this.getAttribute('link-url');
+        
+        const newsContent = document.createElement('p');
+        newsContent.textContent = this.getAttribute('newsContent')
+
+        cardLeft.appendChild(author);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight =  document.createElement('div');
+        cardRight.setAttribute("class", "card__right");
+
+        const newsImage = document.createElement("img")
+        newsImage.src = this.getAttribute('photo') || "assets/default.jpg"
+        newsImage.alt = "Foto da notícia"
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
 
     styles() { }
 }
